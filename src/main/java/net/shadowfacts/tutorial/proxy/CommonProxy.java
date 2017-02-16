5739dd1813f047fc9bf34ed5e5f4e1543dca1fc6
max_givno <max.ryazanov@gmail.com>
2017-02-16 16:11:43 +0200

    Changing usage of deprecated I18n to TextComponentTranslation - to test on server

--- src/main/java/net/shadowfacts/tutorial/proxy/CommonProxy.java
+++ src/main/java/net/shadowfacts/tutorial/proxy/CommonProxy.java
@@ -1,14 +1,18 @@
 package net.shadowfacts.tutorial.proxy;
 
 import net.minecraft.item.Item;
-import net.minecraft.util.text.translation.I18n;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
+//import net.minecraft.util.text.translation.I18n;
 
 public class CommonProxy {
     public void registerItemRenderer(Item item, int meta, String id) {
     }
 
     public String localize(String unlocalized, Object... args) {
-        return I18n.translateToLocalFormatted(unlocalized, args);
+        //return I18n.translateToLocalFormatted(unlocalized, args);
+        ITextComponent text = new TextComponentTranslation(unlocalized, args);
+        return text.getFormattedText();
     }
 
 }
